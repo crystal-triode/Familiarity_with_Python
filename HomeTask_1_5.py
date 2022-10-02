0a2452a29a2b75bcbf9f4eb1c468a479e7b72ea5
crystal-triode <olega2111@yandex.ru>
2022-10-02 18:31:04 +0300

    add HomeTask_2_1

--- HomeTask_1_5.py
+++ HomeTask_1_5.py
@@ -10,6 +10,5 @@ y_A = float(input('Y: '))
 print('Введите координаты точки B:')
 x_B = float(input('X: '))
 y_B = float(input('Y: '))
-
 from math import sqrt
 print('Расстояние между точками A и B: ',round(sqrt((x_A - x_B)**2 + (y_A - y_B)**2), 2))
